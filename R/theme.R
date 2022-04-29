0ed120593b90148d1acb3420161b9e849524dad2
Markus Ljungqvist <markus@ljungqvist.xyz>
2022-04-29 19:34:19 +0200

    nordthemes

--- R/theme.R
+++ R/theme.R
@@ -27,15 +27,21 @@ catppuccin <- data.frame(
 nord <- data.frame(
 				   nord0 = "#2e3440",
 				   nord1 = "#3b4252",
-				   nord3 = "#434c5e",
-				   nord4 = "#4c566a",
-				   nord5 = "#d8dee9",
-				   nord6 = "#e5e9f0",
-				   nord7 = "#eceff4",
-				   nord8 = "#8fbcbb",
-				   nord9 = "#88c0d0",
-				   nord10 = "#81a1c1",
-				   nord11 = "#5e81ac")
+				   nord2 = "#434c5e",
+				   nord3 = "#4c566a",
+				   nord4 = "#d8dee9",
+				   nord5 = "#e5e9f0",
+				   nord6 = "#eceff4",
+				   nord7 = "#8fbcbb",
+				   nord8 = "#88c0d0",
+				   nord9 = "#81a1c1",
+				   nord10 = "#5e81ac",
+				   nord11 = "#bf616a",
+				   nord12 = "#d08770",
+				   nord13 = "#ebcb8b",
+				   nord14 = "#a3be8c",
+				   nord15 = "#b48ead"
+					)
 
 #### Catppuccin Dark Theme ####
 catppuccin_dark <- function(){
@@ -60,5 +66,31 @@ catppuccin_light <- function(){
 				axis.text.x = element_text(color = catppuccin$black2),
 				panel.grid.minor = element_line(color = catppuccin$lavender),
 				panel.grid.major = element_line(color = catppuccin$lavender)
+	)
+}
+
+#### Nord Light Theme ####
+nord_light <- function(){
+	theme <- theme(
+				panel.background = element_rect(fill = nord$nord4),
+				plot.background = element_rect(fill = nord$nord5),
+				axis.title = element_text(color = nord$nord1),
+				axis.text.y = element_text(color = nord$nord1),
+				axis.text.x = element_text(color = nord$nord1),
+				panel.grid.minor = element_line(color = nord$nord4),
+				panel.grid.major = element_line(color = nord$nord4)
+	)
+}
+
+#### Nord Dark Theme ####
+nord_dark <- function(){
+	theme <- theme(
+				panel.background = element_rect(fill = nord$nord0),
+				plot.background = element_rect(fill = nord$nord0),
+				axis.title = element_text(color = nord$nord5),
+				axis.text.y = element_text(color = nord$nord5),
+				axis.text.x = element_text(color = nord$nord5),
+				panel.grid.minor = element_line(color = nord$nord1),
+				panel.grid.major = element_line(color = nord$nord1)
 	)
 }
